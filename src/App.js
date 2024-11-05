1f71f91f4c40c4aea3442ed65507bdfcc57fb7c4
Rodrigo Pereira <rodrigo-q2@hotmail.com>
2024-11-04 22:23:26 -0300

    - alteração texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rodrigo Coladello Pereira
+          Teste publicação react
         </p>
       </header>
     </div>
